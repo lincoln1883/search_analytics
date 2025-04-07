43084f9df9a253300ac259c11fe9c012e60f331a
Lincoln Gibson <lincolngibson7@gmail.com>
2025-04-07 20:19:58 -0300

    Add SearchesController, views, and helper; implement request specs for searches

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
-require 'sidekiq/web'
-
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  root "searches#index"
+  resources :searches, only: [:index, :create]
+  get 'analytics', to: 'analytics#index'
+  
+  require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
