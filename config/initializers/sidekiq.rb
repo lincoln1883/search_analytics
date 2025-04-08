ca07c4794e05c641bf10c2ad03dd64bd2bcb6a24
Lincoln Gibson <lincolngibson7@gmail.com>
2025-04-08 18:44:11 -0300

    Refactor Sidekiq initializer to use Upstash Redis and update Docker Compose for production environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,23 @@
-redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') # Default to local Redis DB 1
+# redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') # Default to local Redis DB 1
+
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: redis_url, network_timeout: 5 }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: redis_url, network_timeout: 5 }
+# end
+require 'upstash/redis'
+
+redis = Upstash::Redis.new(
+  url: ENV["UPSTASH_REDIS_REST_URL"],
+  token: ENV["UPSTASH_REDIS_REST_TOKEN"]
+)
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url, network_timeout: 5 }
+  config.redis = { client: redis }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url, network_timeout: 5 }
+  config.redis = { client: redis }
 end
