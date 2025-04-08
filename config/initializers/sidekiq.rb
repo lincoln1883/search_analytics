80016476465373749699b0a44b1756540a38cc75
Lincoln Gibson <lincolngibson7@gmail.com>
2025-04-08 19:33:59 -0300

    Uncomment Redis gem in Gemfile and update Sidekiq initializer to use ENV variable for Redis URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,15 @@
-# redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') # Default to local Redis DB 1
-
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: redis_url, network_timeout: 5 }
-# end
-
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: redis_url, network_timeout: 5 }
-# end
-require 'upstash/redis'
-
-redis = Upstash::Redis.new(
-  url: ENV["UPSTASH_REDIS_REST_URL"],
-  token: ENV["UPSTASH_REDIS_REST_TOKEN"]
-)
+redis_url = ENV.fetch('REDIS_URL') do
+  if Rails.env.development? || Rails.env.test?
+    'redis://localhost:6379/1' # Local fallback
+  else
+    raise "REDIS_URL environment variable not set!"
+  end
+end
 
 Sidekiq.configure_server do |config|
-  config.redis = { client: redis }
+  config.redis = { url: redis_url, network_timeout: 5 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { client: redis }
+  config.redis = { url: redis_url, network_timeout: 5 }
 end
