938c124797ce50954727595d0b617a26dd46cb0f
Lincoln Gibson <lincolngibson7@gmail.com>
2025-04-08 11:35:22 -0300

    Refactor Sidekiq initializer to remove default Redis URL and add SSL parameters for Redis configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,17 @@
-redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') # Default to local Redis DB 1
+redis_url = ENV.fetch('REDIS_URL')
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url, network_timeout: 5 }
+  config.redis = {
+    url: redis_url,
+    network_timeout: 5,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url, network_timeout: 5 }
+  config.redis = {
+    url: redis_url,
+    network_timeout: 5,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
